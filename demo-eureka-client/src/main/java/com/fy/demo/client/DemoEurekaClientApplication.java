47301294328e729e0a5d41d30461ed5c22ce7743
fangya <fy1005075043@163.com>
2017-10-16 11:15:57 +0800

    fy

--- demo-eureka-client/src/main/java/com/fy/demo/client/DemoEurekaClientApplication.java
+++ demo-eureka-client/src/main/java/com/fy/demo/client/DemoEurekaClientApplication.java
@@ -3,6 +3,7 @@ package com.fy.demo.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by fangya on 2017/10/15.
